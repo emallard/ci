4624444532032878bacf33f4a4259c747b324a88
Etienne <etienne.mallard@gmail.com>
2018-02-27 22:43:03 +0100

    DockerFile

--- cilib/Infrastructure/InstallPilote.sh
+++ cilib/Infrastructure/InstallPilote.sh
@@ -11,10 +11,35 @@ sudo add-apt-repository \
     $(lsb_release -cs) \
     stable"
 
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
 sudo apt-get update
 
-sudo apt-get install docker-ce
+sudo apt-get install docker-ce -y
 
 sudo usermod -aG docker $USER
 
-reboot
+sudo reboot
+
+#docker run hello-world
+
+#recupérer les sources du programme d'init
+sudo apt-get install git
+
+cd ~/
+git clone https://github.com/emallard/ci.git
+cd ~/ci
+git pull
+cd ~/ci/ciexe
+docker build -t ciexe .
+
+
+# récuperer le runtime
+# microsoft/dotnet:2.0-sdk
+# microsoft/dotnet
+
+
+
+
