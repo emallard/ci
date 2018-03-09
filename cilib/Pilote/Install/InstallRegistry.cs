1d82c60f5133bef4329f964e69c7f7877b1e94d2
Etienne <etienne.mallard@gmail.com>
2018-03-10 00:47:48 +0100

    -

--- cilib/Pilote/Install/InstallRegistry.cs
+++ cilib/Pilote/Install/InstallRegistry.cs
@@ -78,8 +78,13 @@ public class InstallRegistry
                 "REGISTRY_HTTP_TLS_KEY=/certs/privateregistry.mynetwork.local.key"
             };
             p.Name = "privateregistry";
-            
+            p.HostConfig = new HostConfig();
+            p.HostConfig.RestartPolicy = new RestartPolicy();
+            p.HostConfig.RestartPolicy.Name = RestartPolicyKind.Always;
+            p.HostConfig.RestartPolicy.MaximumRetryCount = 5;
+
             var response = await client.Containers.CreateContainerAsync(p);
+
 
             var p2 = new ContainerStartParameters();
             await client.Containers.StartContainerAsync(response.ID, p2);
